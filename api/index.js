9736f4a189b5639d494730530ac72a5e44f88449
Selvan S <87435305+Selvan-S@users.noreply.github.com>
2023-12-25 15:42:41 +0530

    new changes

--- api/index.js
+++ api/index.js
@@ -1,3 +1,36 @@
-import app from "../index";
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import { connectDB } from "../db.js";
+import booksRoute from "../routes/booksRoute.js";
+import reviewRoute from "../routes/reviewRoute.js";
+dotenv.config();
+
+const port = process.env.PORT;
+const app = express();
+
+// Middleware for parsing request body
+app.use(express.json());
+
+connectDB();
+
+// Middleware for handling CORS POLICY
+// Option 1: Allow All Origins with Default of cors(*)
+app.use(cors({ origin: "*" }));
+// Option 2: Allow Custom Origins
+// app.use(
+//   cors({
+//     origin: "http://localhost:3000",
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     allowedHeaders: ["Content-Type"],
+//   })
+// );
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
+app.use("/books", booksRoute);
+app.use("/review", reviewRoute);
 
 export default app;
